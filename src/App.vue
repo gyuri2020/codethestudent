3b4332f26566c4305324d821beca0b80cd60ce2c
LeeGyuRi <lj01081512@gmail.com>
2021-11-23 22:51:13 +0900

    Add Footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <Header/>
     <v-app-bar
       app
       color="primary"
@@ -40,17 +41,22 @@
     <v-main>
       <HelloWorld/>
     </v-main>
+    <Footer/>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld';
+import Header from "./components/layout/Header.vue";
+import Footer from "./components/layout/Footer.vue";
 
 export default {
   name: 'App',
 
   components: {
     HelloWorld,
+    Header,
+    Footer,
   },
 
   data: () => ({
---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="text-center">
+  <div>
+    <v-icon>mdi-account-check-outline</v-icon>
+  </div>
+  <footer> 2021-11-23 codethestudent @ copyright</footer>
+</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
